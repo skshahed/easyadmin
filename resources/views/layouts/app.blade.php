0d995632042ca6f090edce5e32e4b96b10c7ab69
skshahed <sha_hed007@yahoo.com>
2024-12-08 18:37:08 +0600

    :Add: New Dashboard design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Template Main CSS File -->
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -21,25 +24,117 @@
         <x-banner />
 
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @livewire('navigation-menu')
+            <div class="container">
+                <!-- =============== Navigation ================ -->
+                <div class="navigation">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <span class="icon">
+                                    <ion-icon name="logo-apple"></ion-icon>
+                                </span>
+                                <span class="title">Brand Name</span>
+                            </a>
+                        </li>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+                        <li>
+                            <a href="#">
+                                <span class="icon">
+                                    <ion-icon name="home-outline"></ion-icon>
+                                </span>
+                                <span class="title">Dashboard</span>
+                            </a>
+                        </li>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                        <li>
+                            <a href="#">
+                                <span class="icon">
+                                    <ion-icon name="people-outline"></ion-icon>
+                                </span>
+                                <span class="title">Customers</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <span class="icon">
+                                    <ion-icon name="chatbubble-outline"></ion-icon>
+                                </span>
+                                <span class="title">Messages</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <span class="icon">
+                                    <ion-icon name="help-outline"></ion-icon>
+                                </span>
+                                <span class="title">Help</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <span class="icon">
+                                    <ion-icon name="settings-outline"></ion-icon>
+                                </span>
+                                <span class="title">Settings</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <span class="icon">
+                                    <ion-icon name="lock-closed-outline"></ion-icon>
+                                </span>
+                                <span class="title">Password</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                <span class="icon">
+                                    <ion-icon name="log-out-outline"></ion-icon>
+                                </span>
+                                <span class="title">Sign Out</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- ========================= Main ==================== -->
+                <div class="main">
+                    @livewire('navigation-menu')
+
+                    <!-- Page Heading -->
+                    {{-- @if (isset($header))
+                        <header class="bg-white dark:bg-gray-800 shadow">
+                            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                                {{ $header }}
+                            </div>
+                        </header>
+                    @endif --}}
+
+                    <!-- Page Content -->
+                    <main>
+                        {{ $slot }}
+                    </main>
+                </div>
+            </div>
         </div>
 
         @stack('modals')
 
         @livewireScripts
+
+        <!-- Template Main JS File -->
+        <script src="{{ asset('js/main.js') }}"></script>
+
+        <!-- ====== ionicons ======= -->
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+        @yield('footer_scripts')
+
     </body>
 </html>
